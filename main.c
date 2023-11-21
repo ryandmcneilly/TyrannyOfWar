8266a88eb41e1d4cc2a6df27a03c36ada903e844
Ryan McNeilly <ryanmcn@live.com.au>
2023-11-21 23:09:39 +1000

    Working on getting textures from atlas file

--- main.c
+++ main.c
@@ -4,11 +4,29 @@
 #define WWIDTH 1000
 #define WHEIGHT 800
 
+enum Animation {
+    STANDING=0,
+    WALKING_LEFT,
+    WALKING_RIGHT,
+    NUM_ANIMATIONS,
+};
+
+Rectangle get_texture_rectangle(enum Animation animation, Texture2D atlas) {
+    Rectangle rectangle = {0};
+
+    return rectangle;
+}
+
 int main() {
     InitWindow(WWIDTH, WHEIGHT, "Tyranny of War");
+
+    Texture2D atlas = LoadTexture("./assets/sprites/Orc-Grunt.png");
+
     while (!WindowShouldClose()) {
         BeginDrawing();
-           DrawText("hello", WWIDTH / 2, WHEIGHT / 2, 20, RED);
+            ClearBackground(WHITE);
+
+            DrawTexture(atlas, 0, 0, WHITE);
         EndDrawing();
     }
     CloseWindow();
