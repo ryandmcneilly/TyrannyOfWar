2b36f33fa7eaace2ad390b8d1fa649791a23ac63
Ryan McNeilly <ryanmcn@live.com.au>
2023-11-23 23:46:11 +1000

    Added debug option which compiles and runs main in gdb.

--- run.sh
+++ run.sh
@@ -5,7 +5,10 @@ INCLUDE="-I/opt/raylib/src"
 LIBS="-L/opt/raylib/src"
 CFLAGS="-Wall -Wpedantic -Wextra"
 LDFLAGS="-lraylib -lGL -lm -lpthread -ldl -lrt"
-gcc main.c -o main $INCLUDE $LIBS $CFLAGS $LDFLAGS
-if [ "$1" != "compile" ]; then
+DEBUGGING="-g"
+gcc main.c -o main $INCLUDE $LIBS $CFLAGS $LDFLAGS $DEBUGGING
+if [ "$1" == "debug" ]; then
+    gdb ./main
+elif [ "$1" != "compile" ]; then
     ./main
 fi
